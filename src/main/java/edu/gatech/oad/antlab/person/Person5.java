22348ebc3f3fbe83f7a9d36fd01b8f53591f99d1
haoranx3 <haoranx3@vt.edu>
2019-05-28 13:19:15 -0400

    modified person5

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Haoran Xin
  *  @version 1.1
  */
 public class Person5 {
@@ -31,7 +31,11 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  //Person 5 put your implementation here
-	  return null;
+		if (input.length() <= 2) {
+			reutrn input;
+		} else {
+			return input.substring(2) + input.substring(0, 2);
+		}
 	}
 	
 	/**
