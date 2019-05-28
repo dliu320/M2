e996e9b2e308b9a7268826de06e766ae2242cde6
Jerry Zhang <jerry@lawn-128-61-123-71.lawn.gatech.edu>
2019-05-28 15:10:56 -0400

    Changed Person2.java

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,5 +1,5 @@
 package edu.gatech.oad.antlab.person;
-
+import java.util.*;
 /**
  *  A simple class for person 2
  *  returns their name and a
@@ -11,12 +11,12 @@ package edu.gatech.oad.antlab.person;
 public class Person2 {
     /** Holds the persons real name */
     private String name;
-	 	/**
+    	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-	 public Person2(String pname) {
+    		public Person2(String pname) {
 	   name = pname;
 	 }
 	/**
@@ -30,8 +30,16 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+		List<Character> characters = new ArrayList<Character>();
+		for(int i = 0; i < input.length(); i++){
+			characters.add(input.charAt(i));
+		}
+		StringBuilder output = new StringBuilder(input.length());
+		while(characters.size()!=0){
+			int randPicker = (int)(Math.random()*characters.size());
+			output.append(characters.remove(randPicker));
+		}
+	  return output.toString();
 	}
 	/**
 	 * Return a string rep of this object
