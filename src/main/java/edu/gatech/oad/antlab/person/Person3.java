77dde1812affc1e92899a244e8c1a9302b3ce125
Jerry Zhang <jerry@lawn-128-61-123-71.lawn.gatech.edu>
2019-05-28 14:50:49 -0400

    modified Person2.java

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -8,7 +8,7 @@ package edu.gatech.oad.antlab.person;
  *  @version 1.1
  */
 public class Person3 {
-   /** Holds the persons real name */  
+   /** Holds the persons real name */
 	private String name;
 	
 	/**
